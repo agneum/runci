50d2f6b19b2a0f93213a77686dcf60efb2a50715
akartasov <agneum@gmail.com>
2021-04-06 12:57:58 +0700

    use inputs

--- migration-action/entrypoint.sh
+++ migration-action/entrypoint.sh
@@ -2,5 +2,16 @@
 
 echo "Owner $1"
 
+JSON_DATA=$(jq -n \
+  --arg owner "$INPUT_OWNER" \
+  --arg repo "$INPUT_REPO" \
+  --arg ref "$INPUT_REF" \
+  '{owner: $owner, repo: $repo, ref: $ref}')
+
+curl --location --request POST '${CI_ENDPOINT}' \
+--header 'Authorization-Token: secret_token' \
+--header 'Content-Type: application/json' \
+--data-raw '${JSON_DATA}'
+
 status="OK"
 echo "::set-output name=status::$status"
