9fb0d7ed661619bbd3ff518c49a873e445fb0a15
akartasov <agneum@gmail.com>
2021-04-06 21:52:39 +0700

    trim newlines

--- migration-action/entrypoint.sh
+++ migration-action/entrypoint.sh
@@ -1,18 +1,16 @@
 #!/bin/sh -l
 
-echo "Owner $1"
-
 JSON_DATA=$(jq -n \
   --arg owner "$INPUT_OWNER" \
   --arg repo "$INPUT_REPO" \
   --arg ref "$INPUT_REF" \
   --arg commands "$INPUT_COMMANDS" \
-  '{owner: $owner, repo: $repo, ref: $ref, commands: $commands | split("\n")}')
+  '{owner: $owner, repo: $repo, ref: $ref, commands: $commands | rtrimstr("\n") | split("\n")}')
 
 echo $JSON_DATA
 
 curl --location --request POST "${CI_ENDPOINT}" \
---header 'Authorization-Token: secret_token' \
+--header 'Authorization-Token: "${SECRET_TOKEN}"' \
 --header 'Content-Type: application/json' \
 --data-raw "${JSON_DATA}"
 
