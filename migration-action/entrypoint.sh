b8b9750dd6b758b6a63906798a6ac9ffa8103fa0
akartasov <agneum@gmail.com>
2021-04-13 16:35:43 +0700

    customize response

--- migration-action/entrypoint.sh
+++ migration-action/entrypoint.sh
@@ -12,10 +12,17 @@ JSON_DATA=$(jq -n -c \
 
 echo $JSON_DATA
 
-response=$(curl -s --location --request POST "${CI_ENDPOINT}" \
+response_code=$(curl --show-error --silent --location --request POST "${CI_ENDPOINT}" --write-out "%{http_code}" \
 --header "Verification-Token: ${SECRET_TOKEN}" \
 --header 'Content-Type: application/json' \
+--output response.json \
 --data "${JSON_DATA}")
 
-echo "Response ${response}"
+if [[ $response_code -ne 200 ]]; then
+  echo "Invalid status code given: ${response_code}"
+  exit 1
+fi
+
+response=$(cat response.json)
+
 echo "::set-output name=response::$response"
