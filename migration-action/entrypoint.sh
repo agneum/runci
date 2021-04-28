ab797e449592e74b9f316c8c7581dcd20bac3816
akartasov <agneum@gmail.com>
2021-04-28 18:33:47 +0700

    check pr

--- migration-action/entrypoint.sh
+++ migration-action/entrypoint.sh
@@ -8,12 +8,14 @@ JSON_DATA=$(jq -n -c \
   --arg db_name "$INPUT_DBNAME" \
   --arg actor "$GITHUB_ACTOR" \
   --arg branch "${GITHUB_REF##*/}" \
+  --arg diff $INPUT_DIFF \
   --arg migration_envs "$INPUT_MIGRATION_ENVS" \
-  '{source: {owner: $owner, repo: $repo, ref: $ref, branch: $branch}, actor: $actor, db_name: $db_name, commands: $commands | rtrimstr("\n") | split("\n"), migration_envs: $migration_envs | rtrimstr("\n") | split("\n")}')
+  '{source: {owner: $owner, repo: $repo, ref: $ref, branch: $branch, diff: $diff}, actor: $actor, db_name: $db_name, commands: $commands | rtrimstr("\n") | split("\n"), migration_envs: $migration_envs | rtrimstr("\n") | split("\n")}')
 
 echo $JSON_DATA
 
 echo $INPUT_DIFF
+echo $GITHUB_HEAD_REF
 echo ${GITHUB_REF##*/}
 
 env
