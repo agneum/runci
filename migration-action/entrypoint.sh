f85f55348ae125645980a1e1dcf10aa1255bde16
Artyom Kartasov <agneum@users.noreply.github.com>
2021-04-28 21:39:45 +0700

    240 debug ci (#4)
    * enrich CI check info

--- migration-action/entrypoint.sh
+++ migration-action/entrypoint.sh
@@ -7,15 +7,26 @@ JSON_DATA=$(jq -n -c \
   --arg commands "$INPUT_COMMANDS" \
   --arg db_name "$INPUT_DBNAME" \
   --arg actor "$GITHUB_ACTOR" \
+  --arg branch "${GITHUB_HEAD_REF:-${GITHUB_REF##*/}}" \
+  --arg commit_sha "${INPUT_COMMIT_SHA}" \
+  --arg commit "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/commit/${INPUT_COMMIT_SHA}" \
+  --arg request_link "${INPUT_PULL_REQUEST:-$INPUT_COMPARE}" \
   --arg migration_envs "$INPUT_MIGRATION_ENVS" \
-  '{source: {owner: $owner, repo: $repo, ref: $ref}, actor: $actor, db_name: $db_name, commands: $commands | rtrimstr("\n") | split("\n"), migration_envs: $migration_envs | rtrimstr("\n") | split("\n")}')
+  '{source: {owner: $owner, repo: $repo, ref: $ref, branch: $branch, commit_sha: $commit_sha, commit: $commit, request_link: $request_link}, actor: $actor, db_name: $db_name, commands: $commands | rtrimstr("\n") | split("\n"), migration_envs: $migration_envs | rtrimstr("\n") | split("\n")}')
 
 echo $JSON_DATA
 
-response=$(curl -s --location --request POST "${CI_ENDPOINT}" \
+response_code=$(curl --show-error --silent --location --request POST "${CI_ENDPOINT}" --write-out "%{http_code}" \
 --header "Verification-Token: ${SECRET_TOKEN}" \
 --header 'Content-Type: application/json' \
+--output response.json \
 --data "${JSON_DATA}")
 
-echo "Response ${response}"
+if [[ $response_code -ne 200 ]]; then
+  echo "Invalid status code given: ${response_code}"
+  exit 1
+fi
+
+response=$(cat response.json)
+
 echo "::set-output name=response::$response"
