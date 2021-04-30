f5158f9423c6b8366ae0628e7ef0d02be05fbfc7
akartasov <agneum@gmail.com>
2021-04-30 16:44:41 +0700

    fix artifacts downloading

--- migration-action/entrypoint.sh
+++ migration-action/entrypoint.sh
@@ -36,24 +36,32 @@ echo $response
 status=$(jq '.session.result.status' response.json)
 echo $status
 
-clone_id=$(jq '.clone_id' response.json)
-echo "CloneID: $status"
+if [[ $status -ne "passed" ]]; then
+  echo "Invalid status given: ${status}"
+  exit 1
+fi
 
-session_id=$(jq '.session.id' response.json)
+clone_id=$(jq '.clone_id' response.json)
+echo "CloneID: $clone_id"
+
+session_id=$(jq '.session.session_id' response.json)
 
 echo ${session_id}
+
+download_artifacts() {
+    artifact_code=$(curl --show-error --silent "${CI_ENDPOINT}/artifact/download?artifact_type=$1&session_id=$2&clone_id=$3" --write-out "%{http_code}" \
+         --header "Verification-Token: ${SECRET_TOKEN}" \
+         --header 'Content-Type: application/json' \
+         --output artifacts/$1)
+
+    echo "Artifact code: $artifact_code"
+    echo "Artifact \"$1\" has been downloaded to the artifacts directory"
+}
 
 cat response.json | jq -c '.session.artifacts[]' | while read artifact; do
     echo "Run $artifact"
     download_artifacts $artifact $session_id $clone_id
 done
 
-function download_artifacts() {
-    curl --show-error --silent "${CI_ENDPOINT}/artifact/download?artifact_type=$1&session_id=$2&clone_id=$3" --write-out "%{http_code}" \
-         --header "Verification-Token: ${SECRET_TOKEN}" \
-         --header 'Content-Type: application/json' \
-         --output artifacts/$1
 
-    echo "Artifact \"$1\" has been downloaded to the artifacts directory"
-}
 echo "::set-output name=response::$response"
