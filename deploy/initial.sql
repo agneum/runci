04659651403b0c580f48b09deeaa84528f2f0bb1
akartasov <agneum@gmail.com>
2021-05-06 18:25:58 +0700

    deploy errors

--- deploy/initial.sql
+++ deploy/initial.sql
@@ -4,32 +4,34 @@ begin;
 
 create table abc (a int);
 
--- create or replace function test_scalability(n int)
---   returns void as
--- $$
--- begin
---   for iter in 0..(n - 1)
---     loop
---       execute 'create table abc_' || iter || ' (a int, b text, c text)';
---
---       for sec in 0..100
---         loop
---           execute 'insert into abc_' || iter || ' values (' || sec || ', md5(random()::text), md5(random()::text))';
---         end loop;
---
---       for sec in 0..2
---         loop
---           execute 'create index idx_abc_' || iter || '_' || sec || '_a on abc_' || iter || ' (a)';
---           execute 'create index idx_abc_' || iter || '_' || sec || '_b on abc_' || iter || ' (b)';
---           execute 'create index idx_abc_' || iter || '_' || sec || '_c on abc_' || iter || ' (c)';
---         end loop;
---
---     end loop;
--- end;
--- $$ language plpgsql;
+select * from def;
 
--- select test_scalability(100);
+create or replace function test_scalability(n int)
+  returns void as
+$$
+begin
+  for iter in 0..(n - 1)
+    loop
+      execute 'create table abc_' || iter || ' (a int, b text, c text)';
 
--- select * from def;
+      for sec in 0..100
+        loop
+          execute 'insert into abc_' || iter || ' values (' || sec || ', md5(random()::text), md5(random()::text))';
+        end loop;
+
+      for sec in 0..2
+        loop
+          execute 'create index idx_abc_' || iter || '_' || sec || '_a on abc_' || iter || ' (a)';
+          execute 'create index idx_abc_' || iter || '_' || sec || '_b on abc_' || iter || ' (b)';
+          execute 'create index idx_abc_' || iter || '_' || sec || '_c on abc_' || iter || ' (c)';
+        end loop;
+
+    end loop;
+end;
+$$ language plpgsql;
+
+select test_scalability(100);
+
+
 
 commit;
